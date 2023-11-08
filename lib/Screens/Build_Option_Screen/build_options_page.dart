08a9959117834e1513ff1831f7d9826bf8c27416
vpart <janvivekariya3@gmail.com>
2023-11-08 20:11:30 +0530

    new

--- lib/Screens/Build_Option_Screen/build_options_page.dart
+++ lib/Screens/Build_Option_Screen/build_options_page.dart
@@ -23,7 +23,31 @@ class _Build_Options_PageState extends State<Build_Options_Page> {
       "option_name": "Carrier Objective",
       "image": "assets/icons/briefcase.png",
     },
-
+    {
+      "id": 3,
+      "option_name": "Personal Details",
+      "image": "assets/icons/account.png",
+    },
+    {
+      "id": 4,
+      "option_name": "Educations",
+      "image": "assets/icons/graduation-cap.png",
+    },
+    {
+      "id": 5,
+      "option_name": "Experience",
+      "image": "assets/icons/logical-thinking.png",
+    },
+    {
+      "id": 6,
+      "option_name": "Technical Skills",
+      "image": "assets/icons/certificate.png",
+    },
+    {
+      "id": 8,
+      "option_name": "Projects",
+      "image": "assets/icons/project-management.png",
+    },
   ];
 
   @override
